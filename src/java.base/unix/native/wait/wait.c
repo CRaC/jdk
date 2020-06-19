a96d79fd8a46d173e707bca58f1a342286734347
Anton Kozlov <akozlov@azul.com>
2020-06-19 17:08:35 +0300

    Forward signals to java after restore

--- src/java.base/unix/native/wait/wait.c
+++ src/java.base/unix/native/wait/wait.c
@@ -1,13 +1,52 @@
 
-#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[]) {
-	int status;
-	pid_t pid = waitpid(-1, &status, 0);
-	if (WIFEXITED(status)) {
-		return WEXITSTATUS(status);
-	}
+#define MSGPREFIX "wait: "
 
-	return 1;
+static int g_pid;
+
+static void sighandler(int sig, siginfo_t *info, void *uc) {
+    if (0 <= g_pid) {
+        kill(g_pid, sig);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char *pidstr = getenv("CRTOOLS_INIT_PID");
+    if (!pidstr) {
+        fprintf(stderr, MSGPREFIX "no CRTOOLS_INIT_PID: signals may not be delivered\n");
+    }
+    g_pid = pidstr ? atoi(pidstr) : -1;
+
+    struct sigaction sigact;
+    sigfillset(&sigact.sa_mask);
+    sigact.sa_flags = SA_SIGINFO;
+    sigact.sa_sigaction = sighandler;
+
+    int sig;
+    for (sig = 1; sig <= 31; ++sig) {
+        if (sig == SIGKILL || sig == SIGSTOP) {
+            continue;
+        }
+        if (-1 == sigaction(sig, &sigact, NULL)) {
+            perror("sigaction");
+        }
+    }
+
+    sigset_t allset;
+    sigfillset(&allset);
+    if (-1 == sigprocmask(SIG_UNBLOCK, &allset, NULL)) {
+        perror(MSGPREFIX "sigprocmask");
+    }
+
+    int status;
+    waitpid(g_pid, &status, 0);
+    if (WIFEXITED(status)) {
+        return WEXITSTATUS(status);
+    }
+
+    return 1;
 }
